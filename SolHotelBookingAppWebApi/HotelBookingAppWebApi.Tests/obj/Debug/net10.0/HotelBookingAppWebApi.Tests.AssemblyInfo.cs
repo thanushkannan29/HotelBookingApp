d695684884b1c901a803903c9437c233b4885602
Thanush Kannan <thanush.k@nafinc.com>
2026-03-31 12:28:20 +0530

    fine-code-coverage

--- SolHotelBookingAppWebApi/HotelBookingAppWebApi.Tests/obj/Debug/net10.0/HotelBookingAppWebApi.Tests.AssemblyInfo.cs
+++ SolHotelBookingAppWebApi/HotelBookingAppWebApi.Tests/obj/Debug/net10.0/HotelBookingAppWebApi.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelBookingAppWebApi.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95851e793fc2e738fbc292913af91d5103a56b09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a14919977439e7a847a622a4c22cfacd155d9895")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelBookingAppWebApi.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelBookingAppWebApi.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SolHotelBookingAppWebApi/HotelBookingAppWebApi/obj/Debug/net10.0/HotelBookingAppWebApi.AssemblyInfo.cs
+++ SolHotelBookingAppWebApi/HotelBookingAppWebApi/obj/Debug/net10.0/HotelBookingAppWebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelBookingAppWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95851e793fc2e738fbc292913af91d5103a56b09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a14919977439e7a847a622a4c22cfacd155d9895")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelBookingAppWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelBookingAppWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
