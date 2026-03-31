79d2a21262be55f8a03e1c534b02183dfa0cdad3
Thanush Kannan <thanush.k@nafinc.com>
2026-04-01 00:03:34 +0530

    program test

--- SolHotelBookingAppWebApi/HotelBookingAppWebApi/obj/Debug/net10.0/HotelBookingAppWebApi.AssemblyInfo.cs
+++ SolHotelBookingAppWebApi/HotelBookingAppWebApi/obj/Debug/net10.0/HotelBookingAppWebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelBookingAppWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6200bfdfbd1f440e4399005f1f3e6a2c548a2629")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62fca487441819706cdb0d02b5fd2921add8929a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelBookingAppWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelBookingAppWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
