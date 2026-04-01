b1d6ea9c025ace89be217c4c5afea78a24ca5e35
Thanush Kannan <thanush.k@nafinc.com>
2026-04-01 16:20:59 +0530

    delete env

--- Fontend-Angular/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://localhost:7208/api',
-  razorpayKeyId: 'rzp_test_SVtcM9b8whLPCh',
-  geminiApiKey: 'AIzaSyD3Kpdd-9m1wnienb3PQ4ZLKJifUk8DOLQ',
-  groqApiKey: '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'
-};
