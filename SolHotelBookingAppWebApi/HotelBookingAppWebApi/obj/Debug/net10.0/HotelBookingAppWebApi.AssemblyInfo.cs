cb108237bcd80e8e8649ca6292165502d2c7b786
Thanush Kannan <thanush.k@nafinc.com>
2026-04-01 09:27:56 +0530

    line coverage testes

--- SolHotelBookingAppWebApi/HotelBookingAppWebApi/obj/Debug/net10.0/HotelBookingAppWebApi.AssemblyInfo.cs
+++ SolHotelBookingAppWebApi/HotelBookingAppWebApi/obj/Debug/net10.0/HotelBookingAppWebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelBookingAppWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c3a67ba3684ce306ee8b4295f5e9685094269dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1224614332b62ceaa212b86fac80b8e3e7431c2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelBookingAppWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelBookingAppWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
